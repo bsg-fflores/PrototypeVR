ef53e72f4b6a1797b0dea04579b386b395420f03
bsg-fflores <ffloresr@bsginstitute.com>
2024-11-08 16:39:05 -0500

    fix: leaderboard

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -7,12 +7,14 @@ namespace Player
     public class PlayerInputHandler : MonoBehaviourPun
     {
         private PlayerScore _playerScore;
+        private string _playerName;
         private void Awake()
         {
             if (photonView.IsMine)
             {
                 _playerScore = GetComponent<PlayerScore>();
                 _playerScore.enabled = true;
+                // _playerName = photonView.Owner.NickName; 
             }
         }
 
@@ -23,12 +25,12 @@ namespace Player
             if (Input.GetKeyDown(KeyCode.E))
             {
                 _playerScore.AddScore(1);
-                Debug.LogError($"Player: {photonView.Owner.NickName} - Score: {_playerScore.score}");
+                Debug.LogError($"Player: {_playerScore.playerName} - Score: {_playerScore.score}");
             }
             if (Input.GetKeyDown(KeyCode.X))
             {
                 _playerScore.RemoveScore(1);
-                Debug.LogError($"Player: {photonView.Owner.NickName} - Score: {_playerScore.score}");
+                Debug.LogError($"Player: {_playerScore.playerName} - Score: {_playerScore.score}");
             }
         }
     }
